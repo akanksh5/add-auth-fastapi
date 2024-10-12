8aff65d741d8f6aaabc101f830c55ee26816f07c
akanksh5 <akankshjagadish@gmail.com>
2024-10-12 16:07:39 -0700

    sqlite integration

--- main.py
+++ main.py
@@ -1,8 +1,43 @@
-from fastapi import FastAPI
+from typing import Annotated
+
+from fastapi import Depends, FastAPI, HTTPException, Query
+from sqlmodel import Field, Session, SQLModel, create_engine, select
+
+
+class User(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    email: str = Field(index=True, unique = True)
+    password: str | None = Field(default=None, index=True)
+
+
+sqlite_file_name = "test.db"
+sqlite_url = f"sqlite:///{sqlite_file_name}"
+connect_args = {"check_same_thread": False}
+engine = create_engine(sqlite_url, connect_args=connect_args)
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+SessionDep = Annotated[Session, Depends(get_session)]
 
 app = FastAPI()
 
+@app.on_event("startup")
+def on_startup():
+    create_db_and_tables()
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.post("/users/")
+def create_user(user: User, session: SessionDep) -> User:
+    session.add(user)
+    try:
+        session.commit()
+        session.refresh(user)
+    except Exception as e:
+        return("Exceptions raised", e)
+    return user
+
+
